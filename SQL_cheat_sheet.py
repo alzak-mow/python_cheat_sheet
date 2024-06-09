1728d11a5f28974e6dff38a1affb46f2b5f7219e
alzak-mow <1943465@gmail.com>
2024-06-09 21:52:30 +0300

    Добавлена шпаргалка по SQL Добавлена шпаргалка по Питон

--- SQL_cheat_sheet.py
+++ SQL_cheat_sheet.py
@@ -3,11 +3,30 @@ import pyodbc
 import pandas as pd
 import sqlalchemy as sa
 
-##Подключаемся к базе данных
+##Подключаемся к базе данных используя pyodbc
 # Specifying the ODBC driver, server name, database, etc. directly
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost,14333;DATABASE=computer;UID=sa;PWD=SQLPaw!2;CHARSET=UTF8')
 # Create a cursor from the connection
 cursor = cnxn.cursor()
+
+##подключиться к базе данных, используя рекомендуемый SQLalchemy
+import pandas as pd
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+dburl = 'mssql://sa:SQLPaw!2@localhost,14333/inc_out?driver=/opt/homebrew/Cellar/freetds/1.4.12/lib/libtdsodbc.so'
+engine = create_engine(dburl)
+connection = engine.connect()
+
+##формат выполнения запросов через SQLalchemy
+query = "SELECT * FROM Income"
+
+with Session(engine) as session:
+    res = connection.execute(sa.text(query))
+    df = pd.DataFrame(res)
+
+df
 
 ##Формат выполнения запросов. - Теперь готовы. Копим запросы :-)
 query = '''
